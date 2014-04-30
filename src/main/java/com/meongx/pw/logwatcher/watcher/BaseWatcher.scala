62d03a8412abec988ed0b0d10dfe58be9d5f36ea
Khucink Ithem <meongx@gmail.com>
2014-04-30 15:29:43 +0800

    BaseWatcher routine to continuously read logfile

--- src/main/java/com/meongx/pw/logwatcher/watcher/BaseWatcher.scala
+++ src/main/java/com/meongx/pw/logwatcher/watcher/BaseWatcher.scala
@@ -1,5 +1,21 @@
 package com.meongx.pw.logwatcher.watcher
 
-class BaseWatcher {
+import scala.actors.Actor
+import java.io.{RandomAccessFile, File}
 
+class BaseWatcher(logfile:String) extends Actor {
+  def act = {
+    val file = new File(logfile)
+    val reader = new RandomAccessFile(file, "r")
+
+    reader.seek(file.length())
+    loop {
+      Option(reader.readLine()) match {
+        case Some(line) => processLine(line)
+        case None => Thread.sleep(250)
+      }
+    }
+  }
+
+  def processLine(line:String) = println(line)
 }
